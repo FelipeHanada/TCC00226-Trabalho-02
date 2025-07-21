e3911f70bad1de399502836137aeea18c1234684
FelipeHanada <146875733+FelipeHanada@users.noreply.github.com>
2025-07-21 16:52:18 -0300

    refactor favorites

---
+++ src/main/java/com/allberfelipe/trabalho_02/controller/FavoriteController.java
@@ -0,0 +1,56 @@
+package com.allberfelipe.trabalho_02.controller;
+
+import com.allberfelipe.trabalho_02.model.Article;
+import com.allberfelipe.trabalho_02.model.PageResult;
+import com.allberfelipe.trabalho_02.model.User;
+import com.allberfelipe.trabalho_02.service.AuthService;
+import com.allberfelipe.trabalho_02.service.FavoriteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("article/favorite")
+public class FavoriteController {
+
+    @Autowired
+    private FavoriteService favoriteService;
+
+    @Autowired
+    private AuthService authService;
+
+    @GetMapping
+    public PageResult<Article> getFavoriteArticles(
+            @RequestParam(value = "token") String token,
+            @RequestParam(value = "page", defaultValue = "0") int pageIndex,
+            @RequestParam(value = "pageSize", defaultValue = "5") int pageSize
+    ) {
+        User loggedUser = authService.validateToken(token);
+
+        Pageable pageable = PageRequest.of(pageIndex, pageSize);
+        Page<Article> page = favoriteService.getFavoriteArticles(loggedUser.getId(), pageable);
+        return new PageResult<>(page);
+    }
+
+    @PostMapping("{article_id}")
+    public void addFavorite(
+            @PathVariable("article_id") long articleId,
+            @RequestParam(value = "token") String token
+    ) {
+        User loggedUser = authService.validateToken(token);
+
+        favoriteService.addFavorite(loggedUser.getId(), articleId);
+    }
+
+    @DeleteMapping("{article_id}")
+    public void removeFavorite(
+            @PathVariable("article_id") long articleId,
+            @RequestParam(value = "token") String token
+    ) {
+        User loggedUser = authService.validateToken(token);
+
+        favoriteService.removeFavorite(loggedUser.getId(), articleId);
+    }
+}
